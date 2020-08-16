6734b29b8e721f43ed86e4f2cb5b7d3558868134
takayama shunya <katonbo1@icloud.com>
2020-08-16 21:38:04 +0900

    application_controller Basic認証設定追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'],
+                               password: ENV['BASIC_AUTH_PASSWORD']
+                               if Rails.env.production? 
 end
