77eb6da4d736ac57122e4894d1ddab36c1371aa2
takayama shunya <katonbo1@icloud.com>
2020-08-20 17:27:38 +0900

    task終了期限追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,15 @@
 class Task < ApplicationRecord
 
+  before_create :expired_add_time
+
   validates :title, :content, presence: true
   validates :title, length: { maximum:50 }
   validates :content, length: { maximum:100 }
+
+  private
+  def expired_add_time
+    now = Time.current
+    self.expired = now.next_month if expired.blank?
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_185604) do
+ActiveRecord::Schema.define(version: 2020_08_20_064916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_185604) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "expired", default: -> { "now()" }, null: false
   end
 
 end
