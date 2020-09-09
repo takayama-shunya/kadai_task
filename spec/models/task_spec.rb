cd279344be0e98c204ee46862679f76de4878604
takayama shunya <katonbo1@icloud.com>
2020-09-09 10:30:37 +0900

    model テスト修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe Task, type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト', user: admin_user)
+        task = Task.new(title: '', content: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(title: '失敗テスト', content: '', user: admin_user)
+        task = Task.new(title: '失敗テスト', content: '')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト', user: admin_user)
+        task = User.first.tasks.build(title: '成功テスト', content: '成功テスト')
         expect(task).to be_valid
       end
     end
